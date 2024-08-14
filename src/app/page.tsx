f8810c29efe6dae70919448aed5fd418bc3251a3
Selloston <aymankader539@gmail.com>
2024-08-14 23:08:08 +0100

    changes
    Signed-off-by: Selloston <aymankader539@gmail.com>

---
+++ src/app/page.tsx
@@ -0,0 +1,39 @@
+import styles from "./page.module.css"
+
+function App()
+{   
+    return(
+        <>
+        <svg  className={styles.svg}>
+            <filter id="fil">
+                <feTurbulence baseFrequency="0.5">
+                    
+                </feTurbulence>
+            </filter>
+            <rect width="100%" height="100%" filter="url(#fil)"></rect>
+        </svg>
+        <header className={styles.header}>
+            <div id={styles.bar}>
+                <button className={styles.buttonbar}>Home</button>
+                <button className={styles.buttonbar1}>Servises</button>
+                <button className={styles.buttonbar1}>Our Projects</button>
+                <button className={styles.buttonbar1}>About US</button>
+                <button className={styles.buttonbarhelp}>HELP</button>
+            </div>
+        </header>
+        <div className={styles.background}>
+            <div  className={styles.rectangle1}>
+
+            </div>
+            <div className={styles.rectangle2}>
+    
+            </div>
+            <div className={styles.rectangle3}>
+    
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default App;
