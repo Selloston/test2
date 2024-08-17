35586671ad28657eb38ba54c42e3af52f579e4f5
Selloston <aymankader539@gmail.com>
2024-08-17 15:02:19 +0100

    chane
    Signed-off-by: Selloston <aymankader539@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,40 @@
-import styles from "./page.module.css"
+"use client";
+
+import React, { useState, useRef } from 'react';
+import styles from "./page.module.css";
 
 function App()
-{   
+{ 
+    let  [positon, setpositon] = useState({ x: 0, y: 0});
+
+    let cursor = useRef(null)
+
+    let dis = {
+        cursor: "block"
+    }
+
+    let w = 40;
+
+    let movemose = (e) =>
+    {
+        setpositon({ x: e.clientX, y: e.clientY });
+    }
+
+    document.addEventListener("mousemove", movemose);
+    document.addEventListener("mouseenter", function(e)
+    {
+        cursor.current.style.display = "block"
+    });
+
+    document.addEventListener("mouseleave", function(e)
+    {
+        cursor.current.style.display = "none"
+    });
+
+
     return(
         <>
-        <svg  className={styles.svg}>
+        <svg className={styles.svg}>
             <filter id="fil">
                 <feTurbulence baseFrequency="0.5">
                     
@@ -21,6 +51,12 @@ function App()
                 <button className={styles.buttonbarhelp}>HELP</button>
             </div>
         </header>
+        <div ref={cursor} style={{'transform' :`translate(${positon.x - w}px, ${positon.y - w}px)`}} className={styles.cursor}>
+
+        </div>
+        <div className={styles.cursorhover}>
+
+        </div>
         <div className={styles.background}>
             <div  className={styles.rectangle1}>
 
