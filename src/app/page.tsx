03c415b5b18e893dae867c426db408a10954aa78
Selloston <aymankader539@gmail.com>
2024-08-17 17:23:39 +0100

    change
    Signed-off-by: Selloston <aymankader539@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,33 +5,6 @@ import styles from "./page.module.css";
 
 function App()
 { 
-    let  [positon, setpositon] = useState({ x: 0, y: 0});
-
-    let cursor = useRef(null)
-
-    let dis = {
-        cursor: "block"
-    }
-
-    let w = 40;
-
-    let movemose = (e) =>
-    {
-        setpositon({ x: e.clientX, y: e.clientY });
-    }
-
-    document.addEventListener("mousemove", movemose);
-    document.addEventListener("mouseenter", function(e)
-    {
-        cursor.current.style.display = "block"
-    });
-
-    document.addEventListener("mouseleave", function(e)
-    {
-        cursor.current.style.display = "none"
-    });
-
-
     return(
         <>
         <svg className={styles.svg}>
@@ -51,12 +24,6 @@ function App()
                 <button className={styles.buttonbarhelp}>HELP</button>
             </div>
         </header>
-        <div ref={cursor} style={{'transform' :`translate(${positon.x - w}px, ${positon.y - w}px)`}} className={styles.cursor}>
-
-        </div>
-        <div className={styles.cursorhover}>
-
-        </div>
         <div className={styles.background}>
             <div  className={styles.rectangle1}>
 
